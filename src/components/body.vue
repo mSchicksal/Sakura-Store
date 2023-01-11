c4d122612d61c1bfcfe7773facadeb32d47f71d5
mSchicksal <alvaro.catalan8@gmail.com>
2023-01-11 00:27:29 -0300

    rb alt

--- src/components/body.vue
+++ src/components/body.vue
@@ -10,7 +10,7 @@
 
                 <div class="col-sm center">
                     <div class="card">
-                        <img class="card-img-top" src="" alt="alt">
+                        <img class="card-img-top" src="" alt="Card image cap">
                         <div class="card-body">
                             <h5 class="card-title">Card title</h5>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
